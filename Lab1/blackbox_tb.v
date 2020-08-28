0451e8ac1752886ed81cb52e70888f7f42037b11
wangshuo <shuow6@illinois.edu>
2020-08-28 18:47:05 -0500

    My first try

--- Lab1/blackbox_tb.v
+++ Lab1/blackbox_tb.v
@@ -1,3 +1,39 @@
 module blackbox_test;
+// these are inputs to "circuit under test"
+   reg s=0;
+   reg g=0;
+   reg f=0;
+  
+   // wires for the outputs of "circuit under test"
+   wire out;
+  
+   // the circuit under test
+   blackbox bb1(i, s, g, f);  
+    
+   initial begin               // initial = run at beginning of simulation
+                               // begin/end = associate block with initial
+      
+      $dumpfile("test.vcd");  // name of dump file to create
+      $dumpvars(0, test);     // record all signals of module "test" and sub-modules
+                              // remember to change "test" to the correct
+                              // module name when writing your own test benches
 
+      // test all input combinations
+      s = 0; g = 0; f = 0; #10
+      s = 0; g = 0; f = 1; #10
+      s = 0; g = 1; f = 0; #10
+      s = 0; g = 1; f = 1; #10
+      s = 1; g = 0; f = 0; #10
+      s = 1; g = 0; f = 1; #10
+      s = 1; g = 1; f = 0; #10
+      s = 1; g = 1; f = 1; #10
+          
+      $finish;        // end the simulation
+   end                      
+   
+   initial begin
+     $display("inputs = s g f  outputs = i");
+     $monitor("inputs = %b %b %b  outputs = %b %b   ",
+              s, g, f, i);
+   end
 endmodule // blackbox_test
--- Lab1/keypad.v
+++ Lab1/keypad.v
@@ -2,5 +2,20 @@ module keypad(valid, number, a, b, c, d, e, f, g);
    output 	valid;
    output [3:0] number;
    input 	a, b, c, d, e, f, g;
-
+   wire w1, w2, w3, w4, w5, w6, w7, w8, w9, w0;
+   and a1(w1, a, d);
+   and a2(w2, b, d);
+   and a3(w3, c, d);
+   and a4(w4, a, e);
+   and a5(w5, b, e);
+   and a6(w6, c, e);
+   and a7(w7, a, f);
+   and a8(w8, b, f);
+   and a9(w9, c, f);
+   and a0(w0, b, g);
+   or ov(valid, w1, w2, w3, w4, w5, w6, w7, w8, w9, w0);
+   or o0(number[0], w1, w3, w5, w7, w9);
+   or o1(number[1], w2, w3, w6, w7);
+   or o2(number[2], w4, w5, w6, w7);
+   or o3(number[3], w8, w9);
 endmodule // keypad
