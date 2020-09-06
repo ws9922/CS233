e82b099751cb501d31b0f826b797c10cf53ff609
wangshuo <shuow6@illinois.edu>
2020-09-06 08:54:26 -0500

    update alu32_tb

--- Lab2/alu32_tb.v
+++ Lab2/alu32_tb.v
@@ -10,17 +10,17 @@ module alu32_test;
              A = 8; B = 4; control = `ALU_ADD; // try adding 8 and 4
         # 10 A = 2; B = 5; control = `ALU_SUB; // try subtracting 5 from 2
         // add more test cases here!
-        # 10 A = 'b10101010101010000; B = 'hffffffff; control = 'b100;
-        # 10 A = 'b10101010101010000; B = 'hffffffff; control = 'b101;
-        # 10 A = 'b10101010101010000; B = 'hffffffff; control = 'b110;
-        # 10 A = 'b10101010101010000; B = 'hffffffff; control = 'b111;
+        # 10 A = 32'b10101010101010000; B = 32'hffffffff; control = 3'b100;
+        # 10 A = 32'b10101010101010000; B = 32'hffffffff; control = 3'b101;
+        # 10 A = 32'b10101010101010000; B = 32'hffffffff; control = 3'b110;
+        # 10 A = 32'b10101010101010000; B = 32'hffffffff; control = 3'b111;
         # 10 A = 49; B = 49; control = `ALU_SUB;
         # 10 A = 36; B = 'h7ffffff0; control = `ALU_SUB;
-        # 10 A = 'h7fffffff; B = 'h00000001; control = `ALU_ADD;
-        # 10 A = 'h7ffffffe; B = 'h00000001; control = `ALU_ADD;
-        # 10 A = 'h80000000; B = 'hffffffff; control = `ALU_ADD;
-        # 10 A = 'h80000001; B = 'hffffffff; control = `ALU_ADD;
-        # 10 A = 'h7fffffff; B = 'hffffffff; control = `ALU_SUB;
+        # 10 A = 32'h7fffffff; B = 32'h00000001; control = `ALU_ADD;
+        # 10 A = 32'h7ffffffe; B = 32'h00000001; control = `ALU_ADD;
+        # 10 A = 32'h80000000; B = 32'hffffffff; control = `ALU_ADD;
+        # 10 A = 32'h80000001; B = 32'hffffffff; control = `ALU_ADD;
+        # 10 A = 32'h7fffffff; B = 32'hffffffff; control = `ALU_SUB;
         # 10 $finish;
     end
 
