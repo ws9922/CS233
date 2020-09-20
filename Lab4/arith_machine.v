3b2ed68b71525659c848d26c26226c538fbfd173
wangshuo <shuow6@illinois.edu>
2020-09-20 16:02:30 -0500

    finsh lab4 part2

--- Lab4/arith_machine.v
+++ Lab4/arith_machine.v
@@ -9,20 +9,46 @@ module arith_machine(except, clock, reset);
     input       clock, reset;
 
     wire [31:0] inst;  
-    wire [31:0] PC;  
+    wire [31:0] PC; 
+    wire [29:0]addr = PC[31:2]; 
+    wire [5:0]opcode = inst[31:26];
+    wire [5:0]funct = inst[5:0];
+    wire [4:0]Rs = inst[25:21];
+    wire [4:0]Rt = inst[20:16];
+    wire [4:0]Rd = inst[15:11];
+    wire [4:0]W_addr;
+    wire [15:0]imm = inst[15:0];
+    wire rd_src, writeenable;
+    wire [1:0] alu_src2;
+    wire [2:0] alu_op;
+    wire [31:0]nextPc;
+    wire [31:0] rsData;
+    wire [31:0] rtData;
+    wire [31:0] rdData;
+    wire [31:0] rtimmeData;
+    wire [31:0] W_data;
+    wire [15:0] sign = {16{imm[15]}};
+    wire [31:0] SignExtender = {sign, imm};
+    wire [31:0] ZeroExtender = {16'b0, imm};
+
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    register #(32) PC_reg( /* connect signals */ );
+    register #(32) PC_reg(PC,nextPc,clock, 1'b1, reset);
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    instruction_memory im( /* connect signals */ );
+    instruction_memory im(inst, addr);
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    regfile rf ( /* connect signals */ );
+    regfile rf (rsData, rtData, Rs, Rt, W_addr, W_data, writeenable, clock, reset);
 
     /* add other modules */
-   
+    mips_decode n1(rd_src, writeenable, alu_src2, alu_op, except, opcode, funct);
+    mux2v m2(W_addr, Rd, Rt, rd_src);
+    mux3v m3(rtimmeData, rtData, SignExtender, ZeroExtender, alu_src2);  
+    alu32 n5(W_data, , , ,rsData, rtimmeData, alu_op);
+    alu32 n6(nextPc, , , , PC, 32'h4, `ALU_ADD);
+    
 endmodule // arith_machine
