bf497f650c070ae9bd819d820163ad0a58695db0
wangshuo <shuow6@illinois.edu>
2020-10-01 14:18:28 -0500

    update lab 6 part1

--- Lab6/p1.s
+++ Lab6/p1.s
@@ -12,33 +12,41 @@
 # }
 .globl fill_array
 fill_array:
-    sub $sp, $sp, 4 
+    sub $sp, $sp, 16 
     sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+    sw $s2, 12($sp)
 
-    li $t0, 0
+    li $s0, 0
 
     for:
-      bgt $t0, 6, end
+      bge $s0, 6, end
       srl $a0, $a0, 3
     
-      rem $t1, $t0, 3
-      bne $t1, $zero, else
-      add $t2, $a1, $t0
-      mul $t2, $t2, 4
-      lw $t2, 0($t2)
-      and $t3, $a0, 0x0000001f
-      sw $t3, 0($t2)
+      rem $s1, $s0, 3
+      bne $s1, $zero, else
+      mul $s2, $s0, 4
+      add $s2, $a1, $s2
+      lw $s3, 0($s2)
+      and $s3, $a0, 0x0000001f
+      sw $s3, 0($s2)
+      add $s0, $s0, 1
       j for
 
     else:
-      add $t2, $a1, $t0
-      mul $t2, $t2, 4
-      lw $t2, 0($t2)
-      and $t3, $a0, 0x000000ff
-      sw $t3, 0($t2)
+      mul $s2, $s0, 4
+      add $s2, $a1, $s2
+      lw $s3, 0($s2)
+      and $s3, $a0, 0x000000ff
+      sw $s3, 0($s2)
+      add $s0, $s0, 1
       j for
 
     end:
       lw $ra, 0($sp)
-      add $sp, $sp, 4
+      lw $s0, 4($sp)
+      lw $s1, 8($sp)
+      lw $s2, 12($sp)
+      add $sp, $sp, 16
       jr $ra
