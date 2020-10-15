20c5699c5d9bf873b3334647528534a999039492
wangshuo <shuow6@illinois.edu>
2020-10-15 12:37:37 -0500

    finish lab8 part1

--- Lab8/timer.v
+++ Lab8/timer.v
@@ -11,4 +11,59 @@ module timer(TimerInterrupt, cycle, TimerAddress,
     // HINT: make your interrupt cycle register reset to 32'hffffffff
     //       (using the reset_value parameter)
     //       to prevent an interrupt being raised the very first cycle
+    wire TimeRead,TimeWrite,Acknowledge,cyccomp;
+    wire resetinterline;
+    wire [31:0] interruptcycle, currentcycle;
+    wire [31:0] outputinterline;
+
+
+
+    //get the result for TimerAddress
+    or n1(TimerAddress, address == 32'hffff001c, address == 32'hffff006c);
+    
+    //get the result for TimeRead
+    and n2(TimeRead, MemRead, address == 32'hffff001c);
+
+    //get the result for TimeWrite
+
+    and n3(TimeWrite,MemWrite,address == 32'hffff001c);
+
+    //get the result for Acknowledge
+    and n4(Acknowledge, address == 32'hffff006c, MemWrite);
+
+    //get the result of interrupt cycle
+    register #(32, 32'hffffffff) n5(interruptcycle, data, clock, TimeWrite, reset);
+
+    
+    wire [31:0] nextcycle;
+    //this is the cycle counter
+
+
+    register n6(currentcycle, nextcycle, clock, 1'b1,reset);
+
+    alu32 n7(nextcycle, , ,`ALU_ADD ,currentcycle, 32'h1);
+
+
+
+    assign cyccomp = (currentcycle == interruptcycle);
+
+    tristate n8(cycle, currentcycle, TimeRead);
+
+    or n9(resetinterline, Acknowledge, reset);
+
+    register n10(outputinterline, 32'h1, clock, cyccomp, resetinterline);
+
+    assign TimerInterrupt = outputinterline[0:0];
+
+
+    
+
+
+    
+
+
+
+
+
+
 endmodule
