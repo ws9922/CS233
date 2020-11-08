7068852310b36da71a54dc244e87133ea3a10673
wangshuo <shuow6@illinois.edu>
2020-11-08 15:55:53 -0600

    finish lab11 part1

--- Lab11/simplecache.cpp
+++ Lab11/simplecache.cpp
@@ -2,10 +2,24 @@
 
 int SimpleCache::find(int index, int tag, int block_offset) {
   // read handout for implementation details
+  std::vector<SimpleCacheBlock> blocks = _cache[index];
+  for (size_t i = 0; i < blocks.size(); i++){
+    if(blocks[i].valid() && blocks[i].tag() == tag){
+      return blocks[i].get_byte(block_offset);
+    }
+  }
   return 0xdeadbeef;
 }
 
 void SimpleCache::insert(int index, int tag, char data[]) {
   // read handout for implementation details
   // keep in mind what happens when you assign (see "C++ Rule of Three")
+  std::vector<SimpleCacheBlock>& blocks = _cache[index];
+  for(size_t i = 0; i < blocks.size(); i++){
+    if(!blocks[i].valid()){
+      blocks[i].replace(tag, data);
+      return;
+    }
+  }
+  blocks[0].replace(tag, data);
 }
